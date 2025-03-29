3dbbfd6b3f8083f0a2e81029fad553c31be62174
naoki <naoki27suzuki1227@gmail.com>
2025-03-29 15:49:31 +0900

    feat: ターゲットタイプ追加

--- alb.tf
+++ alb.tf
@@ -23,6 +23,7 @@ resource "aws_lb_target_group" "tg_gp" {
   port     = 3000
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc.id
+  target_type = "ip"
 
   health_check {
     path                = "/"
